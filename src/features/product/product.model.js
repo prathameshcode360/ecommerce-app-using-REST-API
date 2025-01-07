6c856f9674aaf5d14944978e9d0b679abf7c0e53
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2025-01-07 22:59:33 +0530

    created product model

---
+++ src/features/product/product.model.js
@@ -0,0 +1,28 @@
+export default class ProductModel {
+  constructor(id, name, price, image) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.image = image;
+  }
+}
+var products = [
+  new ProductModel(
+    1,
+    "Product 1",
+    200,
+    "https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg"
+  ),
+  new ProductModel(
+    2,
+    "Product 2",
+    100,
+    "https://m.media-amazon.com/images/I/51xwGSNX-EL._SX356_BO1,204,203,200_.jpg"
+  ),
+  new ProductModel(
+    3,
+    "Product 3",
+    250,
+    "https://m.media-amazon.com/images/I/31PBdo581fL._SX317_BO1,204,203,200_.jpg"
+  ),
+];
