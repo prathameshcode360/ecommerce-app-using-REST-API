3b32e4384cd39acd1e835e638ec42cfe3b771c11
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2025-01-13 19:07:28 +0530

    created product model and successfully get all products on practice1

--- src/features/product/product.controller.js
+++ src/features/product/product.controller.js
@@ -1,3 +1,8 @@
+import ProductModel from "./product.model.js";
+
 export default class ProductController {
-  getAllProducts(req, res) {}
+  getAllProducts(req, res) {
+    let products = ProductModel.getAll();
+    return res.status(200).send(products);
+  }
 }
