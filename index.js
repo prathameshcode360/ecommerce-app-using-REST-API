5d9aaac24ab8a3a6b812f1e3f376fb80c08c234a
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2025-01-14 18:08:30 +0530

    created middleware for jwt token

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import productRouter from "./src/features/product/product.routes.js";
 import userRouter from "./src/features/user/user.routes.js";
-import basicAuth from "./src/middlewares/basic.auth.middleware.js";
+// import basicAuth from "./src/middlewares/basic.auth.middleware.js";
+import jwtAuth from "./src/middlewares/jwtAuth.middleware.js";
 
 const server = express();
 
@@ -13,7 +14,7 @@ server.use(bodyParser.json());
 server.use("/api/user", userRouter);
 
 //creating products routes pattern
-server.use("/api/products", basicAuth, productRouter);
+server.use("/api/products", jwtAuth, productRouter);
 
 server.get("/", (req, res) => {
   res.send("Welcome to node js server");
---
+++ src/middlewares/jwtAuth.middleware.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+export default function jwtAuth(req, res, next) {
+  const token = req.headers["authorization"];
+
+  if (!token) {
+    return res.status(401).send("Unauthorized User");
+  }
+
+  try {
+    const payload = jwt.verify(token, "FAU2McwhilVQHuv7vyIVxpb335HhvhzI");
+    console.log(payload);
+  } catch (err) {
+    return res.status(401).send("unAuthorized User");
+  }
+
+  next();
+}
