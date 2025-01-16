03c0aa789ca8c85eb502e142d5fb25a6b1c6b51f
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2025-01-16 11:06:17 +0530

    added api documentation with swagger UI

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from "express";
+import swagger from "swagger-ui-express";
 import bodyParser from "body-parser";
 import productRouter from "./src/features/product/product.routes.js";
 import userRouter from "./src/features/user/user.routes.js";
 // import basicAuth from "./src/middlewares/basic.auth.middleware.js";
 import jwtAuth from "./src/middlewares/jwtAuth.middleware.js";
 import cartRouter from "./src/features/cart/cart.routes.js";
+import apidocs from "./swagger.json" assert { type: "json" };
 
 const server = express();
+
+// path for swagger ui
+server.use("/api-docs", swagger.serve, swagger.setup(apidocs));
 
 //using body parser middleware
 server.use(bodyParser.json());
