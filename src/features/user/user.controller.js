d6c239c8ecb3fdda9833700bad35b9471a4d6cd9
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2025-01-18 17:59:09 +0530

    password encrypted by using hashing

--- src/features/user/user.controller.js
+++ src/features/user/user.controller.js
@@ -1,5 +1,6 @@
 import UserModel from "./user.model.js";
 import UserRepository from "./user.repository.js";
+import bcrypt from "bcrypt";
 export default class UserController {
   constructor() {
     this.userRepository = new UserRepository();
@@ -7,32 +8,42 @@ export default class UserController {
 
   async register(req, res) {
     const { name, email, password } = req.body;
-    const newUser = new UserModel(name, email, password);
+    const hashPassword = await bcrypt.hash(password, 12);
+    const newUser = new UserModel(name, email, hashPassword);
     await this.userRepository.signUp(newUser);
     return res.status(201).send({
       msg: "user added successfully",
       newuser: newUser,
     });
   }
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+      const findUser = await this.userRepository.findByEmail(email); // Await the asynchronous call
+
+      if (!findUser) {
+        return res.status(400).send("Incorrect Credentials");
+      }
+
+      // Compare the plain password with the hashed password
+      const result = await bcrypt.compare(password, findUser.password);
+
+      if (!result) {
+        return res.status(400).send("Incorrect Credentials");
+      }
+
+      return res.status(200).send({
+        msg: "Login successfully",
+        newuser: findUser,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send("Something went wrong");
+    }
+  }
 
   getUsers(req, res) {
     let users = UserModel.getAll();
     return res.status(200).send(users);
-  }
-  async login(req, res) {
-    try {
-      const { email, password } = req.body;
-      const user = await this.userRepository.signIn(email, password);
-      if (!user) {
-        return res.status(400).send("user not fonud");
-      } else {
-        return res.status(200).send({
-          msg: "login successfully",
-          newuser: user,
-        });
-      }
-    } catch (err) {
-      console.log(err);
-    }
   }
 }
--- src/features/user/user.repository.js
+++ src/features/user/user.repository.js
@@ -4,20 +4,19 @@ export default class UserRepository {
   async signUp(newUser) {
     try {
       const db = getDb();
-      const collection = db.collection("users"); // Corrected method name
-
+      const collection = db.collection("users");
       await collection.insertOne(newUser); // Insert new user into the collection
       return newUser;
     } catch (err) {
       console.log(err);
     }
   }
-  async signIn(email, password) {
+
+  async findByEmail(email) {
     try {
       const db = getDb();
-      const collection = db.collection("users"); // Corrected method name
-
-      return await collection.findOne({ email, password }); // find user into the collection
+      const collection = db.collection("users");
+      return await collection.findOne({ email }); // Find user by email
     } catch (err) {
       console.log(err);
     }
