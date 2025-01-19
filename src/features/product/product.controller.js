84a7ac393fbc68a462f5c7c97b62b2f6c7012d7d
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2025-01-19 09:49:28 +0530

    implemented try catch functinality for best practice on practice2

--- src/features/product/product.controller.js
+++ src/features/product/product.controller.js
@@ -2,32 +2,52 @@ import ProductModel from "./product.model.js";
 
 export default class ProductController {
   getProducts(req, res) {
-    let products = ProductModel.getAll();
-    return res.status(200).send(products);
+    try {
+      let products = ProductModel.getAll();
+      return res.status(200).send(products);
+    } catch (err) {
+      console.error("Error while fetching products:", err);
+      return res.status(500).send("Error occured while fetching products");
+    }
   }
   addProduct(req, res) {
-    const { name, price } = req.body;
-    const image = req.file.filename;
-    const newProduct = ProductModel.add(name, price, image);
-    return res.status(201).send(newProduct);
+    try {
+      const { name, price } = req.body;
+      const image = req.file.filename;
+      const newProduct = ProductModel.add(name, price, image);
+      return res.status(201).send(newProduct);
+    } catch (err) {
+      console.error("Error while fetching products:", err);
+      return res.status(500).send("Error occured while adding products");
+    }
   }
   getOneProduct(req, res) {
-    const id = req.params.id;
-    const product = ProductModel.getOne(id);
-    if (!product) {
-      return res.status(400).send("product not found");
-    } else {
-      return res.status(200).send(product);
+    try {
+      const id = req.params.id;
+      const product = ProductModel.getOne(id);
+      if (!product) {
+        return res.status(400).send("product not found");
+      } else {
+        return res.status(200).send(product);
+      }
+    } catch (err) {
+      console.error("Error while fetching products:", err);
+      return res.status(500).send("Error occured while fetching one product");
     }
   }
   filterProducts(req, res) {
-    const minPrice = req.query.minPrice;
-    const maxPrice = req.query.maxPrice;
-    const result = ProductModel.filter(minPrice, maxPrice);
-    if (!result) {
-      return res.status(400).send("product not found");
-    } else {
-      return res.status(200).send(result);
+    try {
+      const minPrice = req.query.minPrice;
+      const maxPrice = req.query.maxPrice;
+      const result = ProductModel.filter(minPrice, maxPrice);
+      if (!result) {
+        return res.status(400).send("product not found");
+      } else {
+        return res.status(200).send(result);
+      }
+    } catch (err) {
+      console.error("Error while fetching products:", err);
+      return res.status(500).send("Error occured while filtering products");
     }
   }
 }
